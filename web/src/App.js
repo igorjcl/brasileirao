43ee3f3569aadeea7fd94e761e0794bd9ed92b66
Igor José <igorj000@gmail.com>
2020-08-16 22:48:58 -0300

    add loading

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { css } from "@emotion/core";
+import BeatLoader from "react-spinners/BeatLoader";
 import { api } from "./service/api";
 
 import Header from "./components/Header";
@@ -8,12 +10,14 @@ import "./global.css";
 
 function App() {
   const [times, setTimes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const { data } = await api.get();
 
       setTimes(data);
+      setLoading(false);
     };
     fetchData();
   }, []);
@@ -22,20 +26,30 @@ function App() {
     <>
       <Header />
       <div className="container">
-        {times.map(
-          (
-            { position, name, points, defeats, victories, imagePath },
-            index
-          ) => (
-            <Time
-              key={index}
-              name={name}
-              position={position}
-              points={points}
-              defeats={defeats}
-              victories={victories}
-              imagePath={imagePath}
-            />
+        {loading ? (
+          <BeatLoader
+            size={15}
+            color={"#81b910"}
+            css={css`
+              margin-top: 50%;
+            `}
+          />
+        ) : (
+          times.map(
+            (
+              { position, name, points, defeats, victories, imagePath },
+              index
+            ) => (
+              <Time
+                key={index}
+                name={name}
+                position={position}
+                points={points}
+                defeats={defeats}
+                victories={victories}
+                imagePath={imagePath}
+              />
+            )
           )
         )}
       </div>
